ffd2ff443712f52aab3814aaf5e7866526b86a45
tylercsmith94 <tylercsmith94@gmail.com>
2020-09-23 04:30:31 +0700

    Added Yurble images

--- js/app.js
+++ js/app.js
@@ -13,5 +13,5 @@ function startClock() {
 function setRotation(element, rotationRatio) {
   element.style.setProperty('--rotation', rotationRatio * -360)
 }
-setInterval(startClock, 1000)
-startClock()
+// setInterval(startClock, 1000)
+// startClock()

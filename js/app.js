a060c59742c7ffa0a4a6a1e4dd5800175392d769
tylercsmith94 <tylercsmith94@gmail.com>
2020-09-23 03:43:05 +0700

    Added working 1hr timer

---
+++ js/app.js
@@ -0,0 +1,17 @@
+
+const hand = document.querySelector('.minute')
+let timer = 3600;
+
+function startClock() {
+  
+  console.log(timer);
+  const minutesRatio = timer / 3600;
+  setRotation(hand, minutesRatio);
+  timer--;
+}
+
+function setRotation(element, rotationRatio) {
+  element.style.setProperty('--rotation', rotationRatio * -360)
+}
+setInterval(startClock, 1000)
+startClock()

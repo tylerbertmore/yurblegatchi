8780c2ae7e006cf64277268e8b3e2cf43d691c94
tylercsmith94 <tylercsmith94@gmail.com>
2020-09-24 01:14:30 +0700

    Added CSS animation for createpet overlay, added queryselectors

--- js/app.js
+++ js/app.js
@@ -1,5 +1,20 @@
 const hand = document.querySelector('.minute')
 const secretMenuBtn = document.querySelector('.cheat-menu-btn');
+const userName = document.querySelector('h1');
+const petName = document.querySelector('#pet-name');
+
+const hungerStat = document.querySelector('#hunger');
+const sleepStat = document.querySelector('#sleepiness');
+const happinessStat = document.querySelector('#happiness');
+
+console.log(userName);
+console.log(petName);
+console.log(hungerStat);
+console.log(sleepStat);
+console.log(happinessStat);
+
+
+
 let timer = 3600;
 
 function startClock() {
@@ -13,8 +28,8 @@ function startClock() {
 function setRotation(element, rotationRatio) {
   element.style.setProperty('--rotation', rotationRatio * -360)
 }
-setInterval(startClock, 1000)
-startClock()
+// setInterval(startClock, 1000)
+// startClock()
 
 secretMenuBtn.addEventListener('click', function(){
     console.log('found me!');
